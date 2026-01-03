d122808f292573aacc2a5811bc83739fe8fb0d61
Reha <rehareha.ramanana@gmail.com>
2026-01-03 21:03:05 +0100

    Change on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,11 +47,13 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-black overflow-hidden relative">
-      {/* Recording Button - Top Left */}
-      <div className="absolute top-4 left-4 z-50 flex items-center space-x-2">
-        <RecordingButton />
-        <StreamButton />
-      </div>
+      {/* Local (Electron) controls only */}
+      {isElectron && (
+        <div className="absolute top-4 left-4 z-50 flex items-center space-x-2">
+          <RecordingButton />
+          <StreamButton />
+        </div>
+      )}
 
       {videoPath ? (
         <VideoPlayer videoPath={videoPath} />
